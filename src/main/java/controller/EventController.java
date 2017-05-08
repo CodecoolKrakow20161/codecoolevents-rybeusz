0dbee6fc522f73b9db48617639d23638df9435a9
Stefan Kania <stefankania95@gmail.com>
2017-05-08 20:42:56 +0200

    Two methods implemented, renderProducts() and renderProductsByCategory()

--- src/main/java/controller/EventController.java
+++ src/main/java/controller/EventController.java
@@ -1,11 +1,14 @@
 package controller;
 
 import dao.EventDao;
+import model.Event;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -14,9 +17,29 @@ import java.util.Map;
 public class EventController {
     public static ModelAndView renderProducts(Request req, Response res) {
         //Get events from database by Dao
+        EventDao eventDao = new EventDao();
+        List<Event> events = eventDao.getAll();
+        List<String> categories = eventDao.getCategories();
 
         Map params = new HashMap<>();
-        params.put("eventContainer", "Codecool cinema");
+        params.put("eventContainer", events);
+        params.put("categories", categories);
+
         return new ModelAndView(params, "product/index");
     }
+
+    public static ModelAndView renderProductsByCategory(Request req, Response res) {
+        //Get events from database by Dao
+        EventDao eventDao = new EventDao();
+        String category = req.queryParams("category");
+        List<Event> events = eventDao.getByCategory(category);
+        List<String> categories = eventDao.getCategories();
+
+        Map params = new HashMap<>();
+        params.put("eventContainer", events);
+        params.put("categories", categories);
+
+        return new ModelAndView(params, "product/index");
+    }
+
 }
