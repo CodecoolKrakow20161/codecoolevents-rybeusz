3b87e19135486a9c8c8df3698f03d26655625a08
Stefan Kania <stefankania95@gmail.com>
2017-05-14 22:06:19 +0200

    Add EventDao interface

--- src/main/java/dao/EventDao.java
+++ src/main/java/dao/EventDao.java
@@ -2,165 +2,20 @@ package dao;
 
 import model.Event;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by rafalstepien on 28/04/2017.
+ * Created by rybeusz on 14.05.17.
  */
-public class EventDao {
-    public List<Event> getAll() {
-        List<Event> events = new ArrayList<Event>();
+public interface EventDao {
 
-        try {
-            Connection connection = SqliteJDBCConnector.connection();
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery("select * from events");
-            while(rs.next()) {
-                Event event = new Event(
-                        rs.getString("event_name"),
-                        rs.getString("description"),
-                        rs.getString("event_date"),
-                        rs.getString("category")
-                );
-                event.setId(rs.getInt("id"));
-                events.add(event);
-            }
-        } catch(SQLException e) {
-            System.out.println("Connect to DB failed");
-            System.out.println(e.getMessage());
-        }
+    List<Event> getAll();
+    List<Event> getBySearchBox(String eventName);
+    List<Event> getByCategory(String category);
+    List<String> getCategories();
+    Event findEvent(Integer id);
+    void saveEvent(Event event);
+    void removeEvent(Event event);
 
-        return events;
-    }
-
-    public List<Event> getBySearchBox(String event_name) {
-        List<Event> events = new ArrayList<Event>();
-        String sql = "SELECT * FROM events WHERE event_name LIKE (?)";
-        try {
-            Connection connection = SqliteJDBCConnector.connection();
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setString(1,'%' + event_name + '%');
-            ResultSet rs = statement.executeQuery();
-            while (rs.next()) {
-                Event event = new Event(
-                        rs.getString("event_name"),
-                        rs.getString("description"),
-                        rs.getString("event_date"),
-                        rs.getString("category"));
-                event.setId(rs.getInt("id"));
-                events.add(event);
-            }
-            connection.close();
-        } catch(SQLException e) {
-            System.out.println("Connect to DB failed");
-            System.out.println(e.getMessage());
-        }
-        return events;
-    }
-
-    public List<Event> getByCategory(String category) {
-        List<Event> events = new ArrayList<Event>();
-
-        try {
-            Connection connection = SqliteJDBCConnector.connection();
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery("select * from events WHERE category = " + String.format("'%s'", category));
-            while(rs.next()) {
-                Event event = new Event(
-                        rs.getString("event_name"),
-                        rs.getString("description"),
-                        rs.getString("event_date"),
-                        rs.getString("category")
-                );
-                event.setId(rs.getInt("id"));
-                events.add(event);
-            }
-        } catch(SQLException e) {
-            System.out.println("Connect to DB failed");
-            System.out.println(e.getMessage());
-        }
-
-        return events;
-    }
-
-    public List<String> getCategories() {
-        List<String> categories = new ArrayList<String>();
-
-        try {
-            Connection connection = SqliteJDBCConnector.connection();
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery("SELECT category FROM events GROUP BY category");
-            while(rs.next()) {
-                categories.add(rs.getString("category"));
-            }
-        } catch(SQLException e) {
-            System.out.println("Connect to DB failed");
-            System.out.println(e.getMessage());
-        }
-
-        return categories;
-    }
-
-    public void saveEvent(Event event) {
-        String sql;
-        if (event.getId() != null) {
-            sql = "UPDATE events SET event_name=?, description=?, event_date=?, category=? WHERE id=?";
-        } else {
-            sql = "INSERT INTO events(event_name,description,event_date,category) VALUES(?,?,?,?)";
-        }
-        try {
-            Connection connection = SqliteJDBCConnector.connection();
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setString(1,event.getName() );
-            statement.setString(2,event.getDescription() );
-            statement.setString(3,event.getDate() );
-            statement.setString(4,event.getCategory() );
-            if (event.getId() != null) {
-                statement.setInt(5,event.getId());
-            }
-            statement.executeUpdate();
-            connection.close();
-        } catch(SQLException e) {
-            System.out.println("Connect to DB failed");
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public Event findEvent(Integer id) {
-        Event event = null;
-        String sql = "SELECT * FROM events WHERE id = (?)";
-        try {
-            Connection connection = SqliteJDBCConnector.connection();
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setInt(1,id);
-            ResultSet rs = statement.executeQuery();
-            event = new Event(
-                    rs.getString("event_name"),
-                    rs.getString("description"),
-                    rs.getString("event_date"),
-                    rs.getString("category"));
-            event.setId(rs.getInt("id"));
-            connection.close();
-        } catch(SQLException e) {
-            System.out.println("Connect to DB failed");
-            System.out.println(e.getMessage());
-        }
-        return event;
-    }
-
-    public void removeEvent(Event event) {
-        String sql = "DELETE FROM events WHERE id = ?";
-
-        try {
-            Connection connection = SqliteJDBCConnector.connection();
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setInt(1,event.getId());
-            statement.executeUpdate();
-            connection.close();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
 }
+
